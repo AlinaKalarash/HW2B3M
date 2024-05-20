428f39b1cf05ee98eee73d5014e66772bbe8decd
AlinaKalarash <140280974+AlinaKalarash@users.noreply.github.com>
2024-05-20 17:15:36 +0300

    some pull request

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -8,6 +8,7 @@ public class SumCalculator {
             int sum = 0;
             for (int i = 1; i <= n; i++) {
                 sum += i;
+                
             }
             return sum;
         }
